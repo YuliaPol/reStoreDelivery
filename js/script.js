5eb81944f141db19abed1b757dbc05fea11b2a52
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2021-10-25 12:18:43 +0300

    add deliver survet

--- js/script.js
+++ js/script.js
@@ -38,6 +38,18 @@ jQuery(function ($) {
             }, 500);
         });
 
+        //move to the next question
+        $('.deliver-survey').on('change', 'input[type=radio]', function(e){
+            let nextQuestion = $(this).parents('.question-wrap').next();
+            if(nextQuestion.length > 0){
+                setTimeout(() => {
+                    nextQuestion.get(0).scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }, 500);
+            }
+        });
+        
         //height of window
         var intFrameHeight = $(window).height();
         if(intFrameHeight < 601){
@@ -61,6 +73,46 @@ jQuery(function ($) {
         }).on("input", function () {
             this.style.height = "auto";
             this.style.height = (this.scrollHeight) + "px";
+        });
+
+        //validation
+        var formValid = document.getElementsByClassName('form-valid')[0];
+        $('.form-valid').on('click', '.btn-submit', function(e) {
+            $(this).parents('form').submit(function (e) {
+                e.preventDefault();
+                var erroreArrayElemnts = [];
+                var el = document.querySelectorAll('.form-valid input[type="radio"][data-reqired]');
+                for (var i = 0; i < el.length; i++) {
+                    if (el[i].tagName === 'INPUT') {
+                        var name = el[i].getAttribute('name');
+                        if (document.querySelectorAll('[name=' + name + ']:checked').length === 0) {
+                            erroreArrayElemnts.push(el[i]);
+                            $(el[i]).parents('.question-wrap').addClass('has-error');
+                            $(el[i]).parents('.question-wrap').find('.error-text').fadeIn(300);
+                            var inputname = $(el[i]).attr('name');
+                            $('input[name='+ inputname + ']').change(function (e) {
+                                $(this).parents('.question-wrap').removeClass('has-error');
+                                $(this).parents('.question-wrap').find('.error-text').fadeOut(300);
+                            });
+                        }
+                    }
+                }
+
+                if (erroreArrayElemnts.length == 0) {
+                    formValid.submit();
+                }
+                if (erroreArrayElemnts.length > 0) {
+                    console.log('Valid error');
+                    erroreArrayElemnts.sort(function(a, b){
+                        return parseInt($(a).parents('.question-wrap').offset().top)-parseInt($(b).parents('.question-wrap').offset().top)
+                    });
+                    setTimeout(() => {
+                        $(erroreArrayElemnts[0]).parents('.question-wrap').get(0).scrollIntoView({
+                            behavior: 'smooth'
+                        });
+                    }, 100);
+                }
+            });
         });
     });
 });
